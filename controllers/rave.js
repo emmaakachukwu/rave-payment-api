308bedf789683b6fffaa7a95595d7aa2ad11a856
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2021-01-10 16:33:21 +0100

    headers and body

--- controllers/rave.js
+++ controllers/rave.js
@@ -1,30 +1,30 @@
 const axios = require('axios')
 
 exports.postRave = async (req, res) => {
-    // const body = req.body
-    // const headers = req.headers
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'FLWSECK-48e4afbf3e4212654b75e3e92f2333ed-X'
-    }
+    const body = req.body
+    const headers = req.headers
+    // const headers = {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'FLWSECK-48e4afbf3e4212654b75e3e92f2333ed-X'
+    // }
 
-    let body = {
-        "tx_ref": "9876567898",
-        "amount": "50000",
-        "currency": "NGN",
-        "redirect_url": "127.0.0.1/focus",
-        "payment_options": "card",
-        "customer": {
-            "email": "emmanuelakachukwu1@gmail.com",
-            "phonenumber": "08038995822",
-            "name": "Emmanuel Akachukwu"
-        },
-        "customizations": {
-            "title": "100 percent fragrance",
-            "description": "payment for fragrances",
-            "logo": "https://assets.piedpiper.com/logo.png"
-        }
-    }
+    // let body = {
+    //     "tx_ref": "9876567898",
+    //     "amount": "50000",
+    //     "currency": "NGN",
+    //     "redirect_url": "127.0.0.1/focus",
+    //     "payment_options": "card",
+    //     "customer": {
+    //         "email": "emmanuelakachukwu1@gmail.com",
+    //         "phonenumber": "08038995822",
+    //         "name": "Emmanuel Akachukwu"
+    //     },
+    //     "customizations": {
+    //         "title": "100 percent fragrance",
+    //         "description": "payment for fragrances",
+    //         "logo": "https://assets.piedpiper.com/logo.png"
+    //     }
+    // }
 
     try {
         let response = await axios.post("https://api.flutterwave.com/v3/payments", body, {
