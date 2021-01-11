7fbf1c41bd07295995c7f8c8eaec505b6da3d0bc
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2021-01-11 16:14:41 +0100

    verification endpoint

--- controllers/rave.js
+++ controllers/rave.js
@@ -1,25 +1,73 @@
 const axios = require('axios')
 
+const rootUrl = "https://api.flutterwave.com/v3/"
+const host = "api.flutterwave.com"
+
 exports.postRave = async (req, res) => {
     const body = req.body
     const headers = req.headers
-    headers.host = "api.flutterwave.com"
+    headers.host = host
+
+    const url = rootUrl + "payments"
 
     try {
-        let response = await axios.post("https://api.flutterwave.com/v3/payments", body, {
+        let response = await axios.post(url, body, {
             headers
         })
 
         const statusCode = response.status
         const raveStatus = response.data.status
-        res.status(statusCode).send({
+        return res.status(statusCode).send({
             status: statusCode == 200 && raveStatus == 'success' ? true : false,
             data: response.data
         })
     } catch (err) {
-        res.status(401).send({
-            status: false,
-            error: err.message
+        if (err.response != undefined) {
+            return res.status(err.response.status).send({
+                status: false,
+                error: err.response.data
+            })
+        } else {
+            return res.send({
+                status: false,
+                error: err.message
+            })
+        }
+    }
+}
+
+exports.verify = async (req, res) => {
+    const txnId = req.params.txnId
+    const headers = {
+        authorization: req.headers.authorization,
+        'content-type': 'application/json',
+        host
+    }
+
+    const url = rootUrl + "transactions/" + txnId + "/verify"
+
+    try {
+        let response = await axios.get(url, {
+            headers
         })
+
+        const statusCode = response.status
+        const raveStatus = response.data.status
+        return res.status(statusCode).send({
+            status: statusCode == 200 && raveStatus == 'success' ? true : false,
+            data: response.data
+        })
+    } catch (err) {
+        if (err.response != undefined) {
+            return res.status(err.response.status).send({
+                status: false,
+                error: err.response.data
+            })
+        } else {
+            return res.send({
+                status: false,
+                error: err.message
+            })
+        }
     }
 }
--- routes/rave.js
+++ routes/rave.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 const raveController = require("./../controllers/rave")
 
 router.post("/", raveController.postRave)
+router.get("/:txnId", raveController.verify)
 
 module.exports = router
